6f9676236da0f02c3a36856823aba976f9f7e45f
Sai Ongole <svo214@nyu.edu>
2015-07-19 15:52:15 -0400

    Ctrl+c to quit

--- getHigh.py
+++ getHigh.py
@@ -1,18 +1,18 @@
 import random
-print "Smoke it bitches"
+print "Smoke it bitches. Hit x when you're done"
 while(1):
-	inp = raw_input()
-	if(inp != 'x' or inp =="X"):
-		ran = random.random()
-		if(ran>=0 and ran<0.2):
-			print "Draw two bob!"
-		elif(ran>=0.2 and ran<0.4):
-			print "Draw four of them"
-		elif(ran>=0.4 and ran<0.6):
-			print "Not this time my friend!"
-		elif(ran>=0.6 and ran<0.8):
-			print "Reverse it"
-		elif(ran>=0.8 and ran<=1):
-			print "Smoke till you choke!"
+	inp = raw_input() 
+	
+	ran = random.random()
+	if(ran>=0 and ran<0.2):
+		print "Draw two bob!"
+	elif(ran>=0.2 and ran<0.4):
+		print "Draw four of them"
+	elif(ran>=0.4 and ran<0.6):
+		print "Not this time my friend!"
+	elif(ran>=0.6 and ran<0.8):
+		print "Reverse it"
+	elif(ran>=0.8 and ran<=1):
+		print "Smoke till you choke!"
 
 	
